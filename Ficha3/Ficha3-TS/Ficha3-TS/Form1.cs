03ef5d979d46424808a76b5df553ea8193b55097
André Paulo <andre_duivenvoorden@hotmail.com>
2017-03-13 16:29:20 +0000

    ficha3-encripta

--- Ficha3/Ficha3-TS/Ficha3-TS/Form1.cs
+++ Ficha3/Ficha3-TS/Ficha3-TS/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -15,8 +16,27 @@ namespace Ficha3_TS {
             InitializeComponent();
         }
 
+        private byte[] key;
+        private byte[] iv;
+
         private void btEncrypt_Click(object sender, EventArgs e) {
             using(AesCryptoServiceProvider aes = new AesCryptoServiceProvider()) {
+                this.key = aes.Key;
+                this.iv = aes.IV;
+
+                // cifrar
+                // 1º obter os dados a cifrar
+                byte[] dados = Encoding.UTF8.GetBytes(tbEncrypt.Text);
+                // 2º aplicar o algoritmo de símetrico
+                byte[] dadosEnc;
+                using(MemoryStream ms = new MemoryStream()) {
+                    using(CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write)) {
+                        cs.Write(dados, 0, dados.Length);
+                    }
+                    dadosEnc = ms.ToArray();
+                }
+                // 3º mostrar os dados cifrados
+                tbEncrypted.Text = Encoding.UTF8.GetString(dadosEnc);
 
             }
         }
